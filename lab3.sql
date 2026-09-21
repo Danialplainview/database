93c89acd6d84f7a7932af59d5f05a8164e77035f
Danialplainview <da13085@auca.kg>
2026-09-21 15:30:44 +0600

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -1,2 +1,5 @@
+CREATE TABLE students (id SERIAL PRIMARY KEY, name VARCHAR(100), age INT);
 INSERT INTO students (name, age) VALUES ('Alice', 21), ('Bob', 23);
 SELECT * FROM students;
+DELETE FROM students;
+DROP TABLE students;
